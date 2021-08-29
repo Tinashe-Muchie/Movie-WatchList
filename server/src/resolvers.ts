d3fbfee2b44d47338c7ddcfb4cbdcecb66027ad5
Tinashe Muchineripi <tinashemuchineripi@gmail.com>
2021-08-29 16:34:17 +0200

    typescript improvements

--- server/src/resolvers.ts
+++ server/src/resolvers.ts
@@ -11,8 +11,8 @@ const resolvers = {
         },
         /*returns an array of either TvShows or movies depending on the client's input*/
         search: async ( _: unknown, {name}:{name: string}, {dataSources}: {dataSources: any}) => {
-            const data = await dataSources.movieAPI.Search(name);
-            return data.map((item: any) => {
+            const data: MovieIF[] | TvShowIF[] = await dataSources.movieAPI.Search(name);
+            return data.map((item: MovieIF | TvShowIF) => {
                 if(item.media_type === 'movie') return {
                     __typename: 'Movies',
                     ...item
@@ -88,3 +88,38 @@ const resolvers = {
 
 export default resolvers;
 
+interface MovieIF {
+    adult: boolean;
+    backdrop_path: string;
+    genre_ids: number[];
+    id: number;
+    media_type: string;
+    original_language: string;
+    original_title: string;
+    overview: string; 
+    popularity: number;
+    poster_path: string;
+    release_date: string;
+    title: string;
+    video: boolean;
+    vote_average: number;
+    vote_count: number;
+}
+    
+
+interface TvShowIF {
+    backdrop_path: string;
+    first_air_date: string;
+    genre_ids: number[];
+    id: number;
+    media_type: string;
+    name: string;
+    origin_country: Array<string>;
+    original_language: string;
+    original_name: string;
+    overview: string;
+    popularity: number;
+    poster_path: string;
+    vote_average: number;
+    vote_count: number;
+}
