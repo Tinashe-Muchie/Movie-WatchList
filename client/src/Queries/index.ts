247351e367b524da2c6896646f492f3cfd93513f
Tinashe Muchineripi <tinashemuchineripi@gmail.com>
2021-08-30 14:01:29 +0200

    query index

---
+++ client/src/Queries/index.ts
@@ -0,0 +1,6 @@
+export { MOVIES } from './Movies';
+export { SEARCH } from './Search';
+export { TOPRATEDMOVIES } from './TopRatedMovies';
+export { TOPRATEDTvSHOWS } from './TopRatedTvShows';
+export { TvSHOWS } from './TvShows';
+export { UPCOMINGMOVIES } from './UpcomingMovies';
