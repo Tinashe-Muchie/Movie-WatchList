0c34549abc150b1c542052d0075aed5ae1de4272
Tinashe Muchineripi <tinashemuchineripi@gmail.com>
2021-09-04 06:10:55 +0200

    replace queries

--- server/functions/bundle/index.ts
+++
@@ -1,8 +0,0 @@
-import 'dotenv/config';
-import { createLocalServer } from './server';
-
-const server = createLocalServer();
-
-server.listen().then(() => {
-    console.log('server is running on port 4000');
-})
