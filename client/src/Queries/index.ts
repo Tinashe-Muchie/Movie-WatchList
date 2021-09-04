5504ad2be9c8befa4683a195d0324e36dfcd3d95
Tinashe Muchineripi <tinashemuchineripi@gmail.com>
2021-09-04 06:12:44 +0200

    update paths

--- client/src/Queries/index.ts
+++ client/src/Queries/index.ts
@@ -1,6 +1,3 @@
-export { MOVIES } from './Movies';
-export { SEARCH } from './Search';
 export { TOPRATEDMOVIES } from './TopRatedMovies';
 export { TOPRATEDTvSHOWS } from './TopRatedTvShows';
-export { TvSHOWS } from './TvShows';
 export { UPCOMINGMOVIES } from './UpcomingMovies';
